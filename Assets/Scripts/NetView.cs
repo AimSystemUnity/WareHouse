dfb6c3b6f034e51c51cef9b972f4a6eb43a6804c
lokimve7 <lokimve7@gmail.com>
2025-06-02 12:23:02 +0900

    .

--- Assets/Scripts/NetView.cs
+++ Assets/Scripts/NetView.cs
@@ -1,4 +1,7 @@
 ﻿using Newtonsoft.Json.Linq;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 using UnityEngine;
 
 public class NetView : MonoBehaviour
@@ -6,14 +9,32 @@ public class NetView : MonoBehaviour
     // 네트워크 ID
     public long netId;
 
+    static long nextNetId;
+
     protected virtual void Start()
     {
         netId = UDPClient.instance.AddNetView(this);
+        print(name + " : " + netId);
     }
 
     // 서버에서 응답받는 함수
-    public virtual JObject OnMessage(string message) 
+    public virtual JObject OnMessage(string message)
     {
         return JObject.Parse(message);
+    }
+
+    private void OnValidate()
+    {
+#if UNITY_EDITOR
+        if (!EditorApplication.isPlaying && !EditorUtility.IsPersistent(this))
+        {
+
+            if(netId == 0)
+            {
+                netId =  ++nextNetId;
+                print(name + " : " + netId);
+            }
+        }
+#endif
     }
 }
